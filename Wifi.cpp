2b294ee79ee2151c1d0b5b31fd8143a514cc99a2
youssef hikal <mos628551@gmail.com>
2024-11-11 07:20:06 +0200

    Create Wifi.cpp

---
+++ Wifi.cpp
@@ -0,0 +1,23 @@
+void wifiPortal() {
+  display.clearDisplay();
+  display.setCursor(0, 0);
+  display.println("Wi-Fi Portal");
+  display.display();
+  delay(2000);
+}
+
+void wifiDeauth() {
+  display.clearDisplay();
+  display.setCursor(0, 0);
+  display.println("Wi-Fi Deauth Function");
+  display.display();
+  delay(2000);
+}
+
+void wifiScanNetworks() {
+  display.clearDisplay();
+  display.setCursor(0, 0);
+  display.println("Wi-Fi Scan Networks");
+  display.display();
+  delay(2000);
+}
