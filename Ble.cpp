7d397ddc5994e365dd0b03d4a324df8982a357cf
youssef hikal <mos628551@gmail.com>
2024-11-11 07:20:29 +0200

    Create Ble.cpp

---
+++ Ble.cpp
@@ -0,0 +1,15 @@
+void bleJammer() {
+  display.clearDisplay();
+  display.setCursor(0, 0);
+  display.println("BLE Jammer Function");
+  display.display();
+  delay(2000);
+}
+
+void bleSpoof() {
+  display.clearDisplay();
+  display.setCursor(0, 0);
+  display.println("BLE Spoof Function");
+  display.display();
+  delay(2000);
+}
